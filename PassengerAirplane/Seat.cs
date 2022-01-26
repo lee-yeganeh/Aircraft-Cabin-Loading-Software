b68fc259616822e2b04e3a3427626fd62ab74c29
lee-yeganeh <88970020+lee-yeganeh@users.noreply.github.com>
2022-01-26 10:56:52 +0100

    Uploaded project files

---
+++ PassengerAirplane/Seat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace PassengerAirplane
+{
+    public class Seat
+    {
+        private int _seatRow;
+        private char _seatCol;
+        private int _seatStatus;
+
+        public int SeatStatus
+        {
+            get
+            {
+                return _seatStatus;
+            }
+
+            set
+            {
+                _seatStatus = value;
+            }
+        }
+        public int SeatRow
+        {
+            get
+            {
+                return _seatRow;
+            }
+
+            set
+            {
+                _seatRow = value;
+            }
+        }
+        public char SeatCol
+        {
+            get
+            {
+                return _seatCol;
+            }
+
+            set
+            {
+                _seatCol = value;
+            }
+        }
+
+        public Seat(int seatRow, char seatCol, int seatStatus)
+        {
+            this.SeatRow = seatRow;
+            this.SeatCol = seatCol;
+            this.SeatStatus = seatStatus;
+        }
+
+
+
+    }
+}
